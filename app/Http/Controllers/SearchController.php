09f46aaa5cc70d287a4eed61a481f2013a051c72
Pex-Dev <brayanc.moya@gmail.com>
2025-12-23 14:09:56 -0300

    Feature: Added function to show search results

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Quiz;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class SearchController extends Controller
 {
+    public function index(Request $request){
+        $search = $request["search"];
+        $results = Quiz::where('name','LIKE','%'.$search.'%')->paginate(20);
+        return Inertia::render('search/index',[
+            'results'=> $results,
+            'searchText' => $search
+        ]);
+    }
+
     public function dynamicTextSearch(Request $request){
         $search = $request["search"];
         $results = Quiz::where('name','LIKE','%'.$search.'%')->limit(10)->get();
