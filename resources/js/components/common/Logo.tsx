6ff94acb4f1485bed240052f5deac67159c2d552
Pex-Dev <brayanc.moya@gmail.com>
2025-12-31 11:24:25 -0300

    Logo component created

---
+++ resources/js/components/common/Logo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Logo() {
+    return (
+        <div>
+            <h1 className="text-white text-4xl font-bold w-fit rounded-2xl text-shadow-md text-shadow-white/60">
+                <span className="text-amber-400 font-black">Q</span>
+                uizium
+            </h1>
+        </div>
+    );
+}
